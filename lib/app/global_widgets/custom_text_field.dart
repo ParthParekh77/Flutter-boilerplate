ea692c396fcfb4571ab95739291025f2b5c834e4
Parth Parekh <parth.parekh@finoux.com>
2024-07-30 11:44:05 +0530

    custom text file

--- lib/app/global_widgets/custom_text_field.dart
+++ lib/app/global_widgets/custom_text_field.dart
@@ -7,16 +7,14 @@ class CustomTextField extends StatelessWidget {
   final bool obscureText;
   final TextInputType keyboardType;
   final Function(String)? onChanged;
-  final String? initialValue;
   final TextEditingController? controller;
 
-  const CustomTextField({super.key, required this.hintText, this.obscureText = false, this.keyboardType = TextInputType.text, this.onChanged, this.initialValue, this.controller});
+  const CustomTextField({super.key, required this.hintText, this.obscureText = false, this.keyboardType = TextInputType.text, this.onChanged, this.controller});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
-      initialValue: initialValue,
       obscureText: obscureText,
       keyboardType: keyboardType,
       style: AppTextStyles.regular14,
